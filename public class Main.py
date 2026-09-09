802d59f5525fcbac2417f9e5e613a6276400a12d
Lucas10230 <lucas.bugarin@malad.us>
2026-09-08 23:06:34 -0600

    Add files via upload

---
+++ public class Main.py
@@ -0,0 +1,17 @@
+name = "Lucas"
+age = 16
+
+father = "Daniel"
+mother = "Stacy"
+sibling = "Matthew"
+
+print("Hello World")
+print()
+print("My name is", name, "and I am", age, "years old.")
+print()
+print("My parents are:")
+print("Father:", father)
+print("Mother:", mother)
+print()
+print("My sibling is:")
+print(sibling
